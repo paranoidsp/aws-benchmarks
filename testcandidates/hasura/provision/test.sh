d79bb2f236d7cb79569494026d6c9c774fae9895
Karthikeya Viswanath <karthikeya@hasura.io>
2018-07-20 15:07:33 +0530

    Updated

--- testcandidates/hasura/provision/test.sh
+++ testcandidates/hasura/provision/test.sh
@@ -3,7 +3,6 @@
 # Start graphql-engine
 echo "Starting graphql-engine"
 cd ~ec2-user/aws-benchmarks/testcandidates/hasura/provision
-export DATABASE_URL=$(cat ~/postgres_credentials)
-docker build --build-arg "DATABASE_URL=$DATABASE_URL" . -t hasura/graphql-engine-run:latest
-docker run -p 8080:8080 -d hasura/graphql-engine-run:latest
+export DATABASE_URL=$(cat ~/postgres_credentials); docker build --build-arg "DATABASE_URL=$DATABASE_URL" . -t hasura/graphql-engine-run:latest
+docker run -p 8080:8080 -e "DATABASE_URL=$DATABASE_URL" -d hasura/graphql-engine-run:latest
 touch ~/started_docker
