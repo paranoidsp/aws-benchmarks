0c50ef01b29f64edc9026183a5baf5292409967f
Karthikeya Viswanath <karthikeya@hasura.io>
2018-07-23 18:09:40 +0530

    Updated

--- testcandidates/hasura/provision/roles.sql
+++ testcandidates/hasura/provision/roles.sql
@@ -1,8 +1,8 @@
 
 -- Create postgres and admin roles
 
-create user postgres with password "w5V2NGcFWYpn";
+create user postgres with password 'w5V2NGcFWYpn';
 grant rds_superuser to postgres;
 
-create user admin with password "w5V2NGcFWYpn";
+create user admin with password 'w5V2NGcFWYpn';
 grant rds_superuser to admin;
