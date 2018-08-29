a2337f35adbc0daa9487b5c5cc28db0dcdac3818
Karthikeya Viswanath <karthikeya@hasura.io>
2018-08-29 23:01:03 +0530

    Fixed image

--- testcandidates/hasura/provision/test-postgres.sh
+++ testcandidates/hasura/provision/test-postgres.sh
@@ -6,7 +6,7 @@ cpus=$1
 
 cd ~ubuntu/aws-benchmarks/testcandidates/hasura/provision
 
-export DATABASE_URL=$(cat ~/postgres_credentials); docker build . -t hasura/graphql-engine-run:latest
+export DATABASE_URL=$(cat ~/postgres_credentials)
 
 sleep 10
 
@@ -17,6 +17,6 @@ sleep 10
 
 nohup ~ubuntu/aws-benchmarks/get_ram.sh 10800 > ~ubuntu/hasura.ram &
 
-docker run --name graphql-engine -p 8080:8080  -d hasura/graphql-engine-run:dev-rtsopts-n-8406c7b graphql-engine +RTS -N -RTS --database-url "$(cat ~/postgres_credentials)" serve --cors-domain "https://localhost:9695" --server-port 8080 --enable-console -s $cpus -c 50
+docker run --name graphql-engine -p 8080:8080  -d hasura/graphql-engine:dev-rtsopts-n-e924d3e graphql-engine +RTS -N -RTS --database-url "$(cat ~/postgres_credentials)" serve --cors-domain "https://localhost:9695" --server-port 8080 --enable-console -s $cpus -c 50
 cat postgres/metadata.json | curl -d @- -X POST -i http://localhost:8080/v1/query
 touch ~/started_docker
--- testcandidates/hasura/provision/test.sh
+++ testcandidates/hasura/provision/test.sh
@@ -12,6 +12,6 @@ sleep 10
 
 nohup ~ubuntu/aws-benchmarks/get_ram.sh 10800 > ~ubuntu/hasura.ram &
 
-docker run --name graphql-engine -p 8080:8080  -d hasura/graphql-engine:dev-rtsopts-n-8406c7b graphql-engine +RTS -N -RTS --database-url "$(cat ~/postgres_credentials)" serve --cors-domain "https://localhost:9695" --server-port 8080 --enable-console -s $cpus -c 50
+docker run --name graphql-engine -p 8080:8080  -d hasura/graphql-engine:dev-rtsopts-n-e924d3e graphql-engine +RTS -N -RTS --database-url "$(cat ~/postgres_credentials)" serve --cors-domain "https://localhost:9695" --server-port 8080 --enable-console -s $cpus -c 50
 cat postgres/metadata.json | curl -d @- -X POST -i http://localhost:8080/v1/query
 touch ~/started_docker
